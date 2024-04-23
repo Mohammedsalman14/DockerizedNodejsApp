93717fb66e03be8249e5ee3c7d22fad956fd7e63
mohammed salman <mohamedsalman140@gmail.com>
2024-04-23 20:42:51 +0530

    added dockerfile for nodejs app and also in docker-composefile

--- db.js
+++ db.js
@@ -2,14 +2,17 @@
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-    host: 'localhost',
+    host: 'db', // Use service name 'db' as the hostname
     user: 'salman',
     password: 'salman14ms',
     database: 'nodejs_signup'
 });
 
 connection.connect((err) => {
-    if (err) throw err;
+    if (err) {
+        console.error('Error connecting to MySQL:', err.message);
+        return;
+    }
     console.log('Connected to MySQL database');
 });
 
